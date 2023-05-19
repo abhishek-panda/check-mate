4f42317b827ebed88d8aadc7bbae1c3dd0e078be
Abhishek Panda <abhishek.panda@flipkart.com>
2023-05-19 14:52:31 +0530

    commenting to pull request

--- .github/actions/add-pr-comment/action.js
+++ .github/actions/add-pr-comment/action.js
@@ -1,16 +1,1 @@
-const core = require('@actions/core');
-
-try {
-  const input1 = core.getInput('input1');
-  const input2 = core.getInput('input2');
-
-  // JavaScript code for your custom action
-  console.log(`Input 1: ${input1}`);
-  console.log(`Input 2: ${input2 || 'Not provided'}`);
-
-  // Additional actions based on your requirements
-
-  core.setOutput('output', 'Action completed successfully');
-} catch (error) {
-  core.setFailed(error.message);
-}
+console.log("Working");
