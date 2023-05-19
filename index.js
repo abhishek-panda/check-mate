51addb5b9d28bb124b5b37b0361fbe34b936a89d
Abhishek Panda <abhishek.panda@flipkart.com>
2023-05-19 16:36:54 +0530

    commenting to pull request

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-async function run() {
-    try {
-      const owner = core.getInput('owner', { required: true });
-      const repo = core.getInput('repo', { required: true });
-      const pr_number = core.getInput('pr_number', { required: true });
-      const token = core.getInput('token', { required: true });
-  
-      const octokit = new github.getOctokit(token);
-  
-      await octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: pr_number,
-        body: `Hello Working finally`
-      });
-    } catch (error) {
-      core.setFailed(error.message);
-    }
-  }
-  
-  run();
+console.log("Welcome");
+console.log("Some more messages");
