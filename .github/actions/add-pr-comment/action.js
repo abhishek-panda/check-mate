0975b83494dd6bc4414022a71cedbdb6cf207d3c
Abhishek Panda <abhishek.panda@flipkart.com>
2023-05-19 18:24:06 +0530

    commenting to pull request

--- .github/actions/add-pr-comment/action.js
+++ .github/actions/add-pr-comment/action.js
@@ -19,7 +19,7 @@ async function run() {
           commit_sha: sha,
       });
   
-      console.log(github.context.issue.number);
+      console.log(JSON.stringify(github.context));
       const prs = result.data.filter((el) => state === 'all' || el.state === state);
       const pr =
           prs.find((el) => {
