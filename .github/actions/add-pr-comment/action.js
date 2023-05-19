aa14709d430ec1f8a11482258bd91d208fbcaa4b
Abhishek Panda <abhishek.panda@flipkart.com>
2023-05-19 17:17:05 +0530

    commenting to pull request

--- .github/actions/add-pr-comment/action.js
+++ .github/actions/add-pr-comment/action.js
@@ -1,25 +1,25 @@
-// const core = require('@actions/core');
-// const github = require('@actions/github');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-// async function run() {
-//     try {
-//       const owner = core.getInput('owner', { required: true });
-//       const repo = core.getInput('repo', { required: true });
-//       const token = core.getInput('token', { required: true });
+async function run() {
+    try {
+      const owner = core.getInput('owner', { required: true });
+      const repo = core.getInput('repo', { required: true });
+      const token = core.getInput('token', { required: true });
   
-//       const octokit = new github.getOctokit(token);
+      const octokit = new github.getOctokit(token);
   
-//       await octokit.rest.issues.createComment({
-//         owner,
-//         repo,
-//         issue_number: '',
-//         body: `Hello Working finally`
-//       });
-//     } catch (error) {
-//       core.setFailed(error.message);
-//     }
-// }
+      await octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: '',
+        body: `Hello Working finally`
+      });
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+}
   
-// run();
+run();
 
 console.log("Working..........")
